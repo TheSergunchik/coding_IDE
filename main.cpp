9d5e7a93a33a7fed55dd9351f5f9ec9c6113646d
luke-ackerman <luke.ackerman01@gmail.com>
2021-02-24 17:10:32 -0500

    moved everything from main into playGame() except the inputs for codeLength and rangeDigits as well as the new mastermind object and running playGame()

--- main.cpp
+++ main.cpp
@@ -40,51 +40,6 @@ int main()
         cin >> digitRange;
     }while (!cin || digitRange <= 0);
 
-    // If both code length and range are successfully established, creates two code class objects. One is the
-    // secret code, another is the guess code.
-    code secret = code(codeLength, digitRange);
-    code guess = code(codeLength, digitRange);
-
-    do{ //Starts the 10 attempts.
-        cout << endl << "-- Attempt #" << attempts << " --" << endl << endl;
-
-        // Resets the guess vector to get rid of automatically assigned random code. Then prompts user to enter elements
-        // to populate the vector.
-        guess.resetVector();
-        guess.enterElements();
-
-        // Prints both secret and guess code for grading purposes.
-        cout << endl << "SECRET CODE:  ";
-        secret.printVector();
-        cout << "GUESS CODE:   ";
-        guess.printVector();
-        cout << "-------------------" << endl;
-        cout << "Correct Digits:   " << secret.checkCorrect(guess) << endl;
-        cout << "Misplaced Digits: " << secret.checkIncorrect(guess) << endl;
-
-        //Checks to see if all the digits are in all the correct spots. If so, the code is successfully guessed!
-        if(secret.checkCorrect(guess) == codeLength)
-        {
-            solutionFound = true;
-        }
-
-        attempts++;
-    } while (((attempts <= 10) && (!solutionFound)));
-
-    cout << endl << "-- GAME FINISHED --" << endl;
-
-    // Provides two options: one where the game maker wins and one where the game breaker wins, depending if the
-    // game breaker guessed the code in 10 attempts or less.
-    if(solutionFound)
-    {
-        cout << "The code breaker wins!" << endl << "The code was: ";
-        secret.printVector();
-    } else
-    {
-        cout << "The code maker wins!" << endl << "The code was: ";
-        secret.printVector();
-    }
-    
     //create new mastermind object
     mastermind M = mastermind(codeLength, digitRange);
     
--- mastermind.cpp
+++ mastermind.cpp
@@ -55,9 +55,51 @@ bool mastermind::isSolved(const response &A)
 void mastermind::playGame()
 {
     // initializes a random code
-    code secretCode = code(codeLength, digitRange);
-
+    secretCode = code(codeLength, digitRange);
     printSecretCode();
+
+    // If both code length and range are successfully established, creates two code class objects. One is the
+    // secret code, another is the guess code.
+    code secret = code(codeLength, digitRange);
+    code guess = code(codeLength, digitRange);
+
+    int attempts = 1;
+    bool endGame = false;
+
+    do{ //Starts the 10 attempts.
+        cout << endl << "-- Attempt #" << attempts << " --" << endl << endl;
+
+        guess = humanGuess();
+
+        // Prints both secret and guess code for grading purposes.
+        cout << endl << "SECRET CODE:  ";
+        secret.printVector();
+        cout << "GUESS CODE:   ";
+        guess.printVector();
+        cout << "-------------------" << endl;
+        cout << "Correct Digits:   " << getResponse(guess).getCorrectResponse() << endl;
+        cout << "Misplaced Digits: " << getResponse(guess).getIncorrectResponse() << endl;
+
+        //Checks to see if all the digits are in all the correct spots. If so, the code is successfully guessed!
+        endGame = isSolved(getResponse(guess));
+
+        attempts++;
+    } while (((attempts <= 10) && (!endGame)));
+
+    cout << endl << "-- GAME FINISHED --" << endl;
+
+    // Provides two options: one where the game maker wins and one where the game breaker wins, depending if the
+    // game breaker guessed the code in 10 attempts or less.
+    if(endGame)
+    {
+        cout << "The code breaker wins!" << endl << "The code was: ";
+        secret.printVector();
+    } else
+    {
+        cout << "The code maker wins!" << endl << "The code was: ";
+        secret.printVector();
+    }
+
 
     // iteratively gets a guess from the user and prints the response until either the codemaker or the codebreaker has won.
 
